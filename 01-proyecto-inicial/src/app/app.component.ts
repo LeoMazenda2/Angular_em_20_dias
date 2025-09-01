9625490f285815df05f6fa1b78985600973f6bfd
lmazenda <leonildo.mazenda@xdsoftware.ao>
2025-09-01 15:59:01 +0100

    applyed first button

--- 01-proyecto-inicial/src/app/app.component.ts
+++ 01-proyecto-inicial/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CabecalhoComponent } from "./cabecalho/cabecalho.component";
+import { UsuarioComponent } from "./usuario/usuario.component";
 
 @Component({
   selector: 'app-raiz',
   standalone: true,
-  imports: [CabecalhoComponent],
+  imports: [CabecalhoComponent, UsuarioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
