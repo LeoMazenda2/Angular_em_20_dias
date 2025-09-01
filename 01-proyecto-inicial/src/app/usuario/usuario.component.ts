2d627677eee720cc7474125e5e98a38d029492ad
lmazenda <leonildo.mazenda@xdsoftware.ao>
2025-09-01 17:08:03 +0100

    fim do dia

--- 01-proyecto-inicial/src/app/usuario/usuario.component.ts
+++ 01-proyecto-inicial/src/app/usuario/usuario.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 
+import {USUARIOS_FALSOS} from '../usuario/usuarios-falsos';
+const indiceAleatorio = Math.floor(Math.random() * USUARIOS_FALSOS.length);
+
 @Component({
   selector: 'app-usuario',
   standalone: true,
   imports: [],
-  templateUrl: './usuario.component.html',
+templateUrl: './usuario.component.html',
   styleUrl: './usuario.component.css'
 })
 export class UsuarioComponent {
-
+  public usuarioSelecionado = USUARIOS_FALSOS[indiceAleatorio];
 }
+
