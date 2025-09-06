5a965a856c39cdc892ce6465b11861091099a3ae
LeoMazenda2 <leonildo.mazenda@hotmail.com>
2025-09-06 07:42:57 +0100

    Using Signals

--- 01-proyecto-inicial/src/app/usuario/usuario.component.ts
+++ 01-proyecto-inicial/src/app/usuario/usuario.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 import {USUARIOS_FALSOS} from '../usuario/usuarios-falsos';
 const indiceAleatorio = Math.floor(Math.random() * USUARIOS_FALSOS.length);
@@ -7,19 +7,20 @@ const indiceAleatorio = Math.floor(Math.random() * USUARIOS_FALSOS.length);
   selector: 'app-usuario',
   standalone: true,
   imports: [],
-templateUrl: './usuario.component.html',
+  templateUrl: './usuario.component.html',
   styleUrl: './usuario.component.css'
 })
 export class UsuarioComponent {
-  public usuarioSelecionado = USUARIOS_FALSOS[indiceAleatorio];
+  public usuarioSelecionado = signal(USUARIOS_FALSOS[indiceAleatorio]);
+  rotaImagem = computed(() => 'assets/usuarios/' + this.usuarioSelecionado().avatar)
 
-  get rotaImagem(){
-    return 'assets/usuarios/' + this.usuarioSelecionado.avatar;
-  }
+  // get rotaImagem(){
+  //   return 'assets/usuarios/' + this.usuarioSelecionado.avatar;
+  // }
 
   aoSelecionarUsuario(){
     const indiceAleatorio = Math.floor(Math.random() * USUARIOS_FALSOS.length);
-    this.usuarioSelecionado = USUARIOS_FALSOS[indiceAleatorio];
+    this.usuarioSelecionado.set(USUARIOS_FALSOS[indiceAleatorio]);
   }
 }
 
