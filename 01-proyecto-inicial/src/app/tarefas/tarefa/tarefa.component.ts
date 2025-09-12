e05d9623a09ffd31a05e7e17ba7b4426708735af
lmazenda <leonildo.mazenda@xdsoftware.ao>
2025-09-12 12:54:54 +0100

    começar a usar Pipe

--- 01-proyecto-inicial/src/app/tarefas/tarefa/tarefa.component.ts
+++ 01-proyecto-inicial/src/app/tarefas/tarefa/tarefa.component.ts
@@ -1,12 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { type Tarefa } from './tarefa.model';
 import { CartaoComponent } from "../../compartilhado/cartao/cartao.component";
-
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-tarefa',
   standalone: true,
-  imports: [CartaoComponent],
+  imports: [CartaoComponent, DatePipe],
   templateUrl: './tarefa.component.html',
   styleUrl: './tarefa.component.css',
 })
