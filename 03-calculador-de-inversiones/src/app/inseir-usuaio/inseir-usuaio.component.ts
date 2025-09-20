b6afa12975f29468ddf1450043b8632370f84b7b
Leonildo Mazenda <leonildo.mazenda@hotmail.com>
2025-09-21 00:10:18 +0100

    Add form handling and workspace configuration updates
    Added Angular form handling to `inseir-usuaio.component`:
    - Bound `ngSubmit` to the new `aoEnviar()` method.
    - Changed button type to `submit` for form submission.
    - Imported `FormsModule` for form functionality.
    
    Updated workspace configuration:
    - Added `DocumentLayout.json` and `VSWorkspaceState.json`.
    - Modified `slnx.sqlite`.
    
    Added binary files `0e1279e8-8739-4f66-87e6-ea4324da28ac.vsidx` and `.wsuo`.

--- 03-calculador-de-inversiones/src/app/inseir-usuaio/inseir-usuaio.component.ts
+++ 03-calculador-de-inversiones/src/app/inseir-usuaio/inseir-usuaio.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-inseir-usuaio',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './inseir-usuaio.component.html',
   styleUrl: './inseir-usuaio.component.css'
 })
 export class InseirUsuaioComponent {
 
+
+  aoEnviar(){
+    console.log('Envio envidado')
+  }
 }
