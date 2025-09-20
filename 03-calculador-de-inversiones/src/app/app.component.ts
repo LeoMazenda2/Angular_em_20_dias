9bbccb8f2f8beb73aa7ce8f3ecf85e9aab1f8582
Leonildo Mazenda <leonildo.mazenda@hotmail.com>
2025-09-21 00:04:09 +0100

    Add new components and database schema updates
    Introduced `CabecalhoComponent` and `InseirUsuaioComponent` to enhance the Angular application's UI. Updated `app.component.html` and `app.component.ts` to integrate these components.
    
    Implemented significant database schema changes, including new tables (`SymbolDefinition`, `FileScannerCache`, etc.), indexes, and a recursive view (`FullPathEntities`) to manage file system entities and relationships.
    
    Added new assets (`usuario-6.png`, `usuario-5.png`, etc.) and updated project configuration files (`tsconfig.json`, `angular.json`, etc.) to support the new structure. Enhanced documentation in `README.md` and related files.

--- 03-calculador-de-inversiones/src/app/app.component.ts
+++ 03-calculador-de-inversiones/src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { CabecalhoComponent } from "./cabecalho/cabecalho.component";
+import { InseirUsuaioComponent } from "./inseir-usuaio/inseir-usuaio.component";
 
 @Component({
   selector: 'app-raiz',
   standalone: true,
   templateUrl: './app.component.html',
+  imports: [CabecalhoComponent, InseirUsuaioComponent],
 })
 export class AppComponent {}
---
+++ 03-calculador-de-inversiones/src/app/inseir-usuaio/inseir-usuaio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inseir-usuaio',
+  standalone: true,
+  imports: [],
+  templateUrl: './inseir-usuaio.component.html',
+  styleUrl: './inseir-usuaio.component.css'
+})
+export class InseirUsuaioComponent {
+
+}
