a81d34a0fec23f4049ba1bebe88d75c301b90262
lmazenda <leonildo.mazenda@xdsoftware.ao>
2025-09-24 15:40:03 +0100

    aplicando currency em outras moédas

--- 03-calculador-de-inversiones/src/app/resultado-inversion/resultado-inversion.component.ts
+++ 03-calculador-de-inversiones/src/app/resultado-inversion/resultado-inversion.component.ts
@@ -1,9 +1,10 @@
+import { CurrencyPipe } from '@angular/common';
 import { Component, Input, input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-resultado-inversion',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './resultado-inversion.component.html',
   styleUrl: './resultado-inversion.component.css',
 })
